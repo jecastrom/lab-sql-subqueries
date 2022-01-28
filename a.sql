d6c48c1abb4bffb181079f20df1a9061f1e4f9d1
jecastrom <jcastro.de@outlook.com>
2022-01-28 18:40:24 +0100

    s

--- a.sql
+++ a.sql
@@ -1,12 +1,59 @@
 SELECT
-    c.customer_id,
-    concat(c.first_name, ' ', c.last_name) AS customer_name,
-    c.email,
-    co.country
+    *
 FROM
-    customer c
-    INNER JOIN address a ON c.address_id = a.address_id
-    INNER JOIN city ci ON a.city_id = ci.city_id
-    INNER JOIN country co ON ci.country_id = co.country_id
-WHERE
-    country = 'Canada';
+    (
+        SELECT
+            title AS film_title
+        FROM
+            film
+        WHERE
+            film_id IN (
+                SELECT
+                    film_id
+                FROM
+                    film_actor
+                WHERE
+                    actor_id = (
+                        SELECT
+                            actor_id
+                        FROM
+                            (
+                                SELECT
+                                    actor_id,
+                                    count(film_id) AS number_of_films
+                                FROM
+                                    film_actor
+                                GROUP BY
+                                    1
+                                ORDER BY
+                                    2 DESC
+                                LIMIT
+                                    1
+                            ) t
+                    )
+            )
+    ) t0, (
+        SELECT
+            concat(first_name, ' ', last_name) AS most_prolific_actor_name
+        FROM
+            actor
+        WHERE
+            actor_id = (
+                SELECT
+                    actor_id
+                FROM
+                    (
+                        SELECT
+                            actor_id,
+                            count(film_id) AS number_of_films
+                        FROM
+                            film_actor
+                        GROUP BY
+                            1
+                        ORDER BY
+                            2 DESC
+                        LIMIT
+                            1
+                    ) t1
+            )
+    ) t2;
